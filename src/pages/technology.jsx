28edb7c3b291b8a8d5e0836a9b5689f10ec0de49
samkankrale <shubham@rudhisoft.com>
2026-05-26 12:14:55 +0530

    case-sensitive error resolved for server

--- src/pages/technology.jsx
+++ src/pages/technology.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import './Technology.css';
+import './technology.css';
 
 // ── Data ────────────────────────────────────────────────────────────────────
 
